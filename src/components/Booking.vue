0c93dcfb2bef1eaab1483522cb6a53241c2f9ed6
Moslem-Elo <moslemeo@icloud.com>
2025-04-30 17:00:11 +0200

    initial Commit

---
+++ src/components/Booking.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { ref } from 'vue'
+
+const show = ref(false)
+</script>
+
+<template>
+  <v-card class="mx-auto" max-width="344" theme="dark">
+    <v-img
+      height="200px"
+      src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+      cover
+    ></v-img>
+
+    <v-card-title> Osama </v-card-title>
+
+    <v-card-subtitle> 35 Jahre </v-card-subtitle>
+
+    <v-card-actions>
+      <v-btn color="orange-lighten-2" text="Explore"></v-btn>
+
+      <v-spacer></v-spacer>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<style scoped lang="sass">
+
+</style>
